9aa261ec7609a068385aefd456f5792e94b9aba2
Jay <j.adeyina@gmail.com>
2023-06-02 23:08:51 +0900

    Added assets folder

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
   render() {
-    return(
+    return (
       <div>
         <h1>Test</h1>
       </div>
-    )
+    );
   }
 }
 
 const rootElement = document.querySelector("#root");
 ReactDOM.render(<App />, rootElement);
+
