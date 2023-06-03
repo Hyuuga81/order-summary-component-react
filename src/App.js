a1c50ae31b40c1be5a97d8d81ac51351886f93f6
Jay <j.adeyina@gmail.com>
2023-06-04 03:40:48 +0900

    Added jsx

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
+import React, {Component} from "react";
 
+class App extends Component {
+    render() {
+      return (
+        <main>
+            <section>
+                <p>.</p>
+            </section>
+            <section>
+                <h1>Order Summary</h1>
+                <p>You can now listen to millions of songs, audiobooks, and podcasts on  any device anywhere you like!</p>
+                <div>
+                    <div>
+                        <h2>Annual Plan</h2>
+                        <p>$59.99/year</p>
+                    </div>
+                    <a href="#">Change</a>
+                </div>
+
+                <button>Proceed to Payment</button>
+                <h3>Cancel Order</h3>
+            </section>
+        </main>
+      );
+    }
+  }
+
+  export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import App from "./App.js";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Test</h1>
-      </div>
-    );
-  }
-}
+
 
 const rootElement = document.querySelector("#root");
 ReactDOM.render(<App />, rootElement);
